669eb9b52dfedd99e30dde7e70d8f5f7b9e49c0b
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2022-02-24 13:06:36 +0200

    upload project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+jQuery(function ($) {
+    $(document).ready(function () {
+        //width of scroling line
+        function setScrollWisth(){
+            var s = $(window).scrollTop(),
+            d = $(document).height(),
+            c = $(window).height();
+            var scrollPercent = Math.round((s / (d - c)) * 100);
+            if($('.progress-bar .proggres')){
+                $('.progress-bar .proggres').css('width', scrollPercent + '%');
+            }
+        }
+        setScrollWisth();
+        //when page is scrolled
+        $( window ).scroll(function() {
+            setScrollWisth();
+        });
+        // preloader
+        $('.load-wrapper').fadeOut();
+    });
+});
